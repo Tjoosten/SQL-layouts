8963a58b4d13d0c525ff4ce1b42c7e2021bf163d
Tim Joosten <Tjoosten@users.noreply.github.com>
2014-10-22 13:24:17 +0200

    Update Raspberry commands.sql

--- Raspberry commands.sql
+++ Raspberry commands.sql
@@ -1,4 +1,13 @@
+-- Database: RaspberryPI_commands
+-- Engine: MySQL
+-- Author: Tim Joosten
+-- Hosted on Github
+
 CREATE DATABASE IF NOT EXISTS RaspberryPI_commands
 
 CREATE TABLE IF NOT EXISTS Commands (
   ID int(11) AUTO INCREMENT,
+  Command VARCHAR(255),
+  Description TEXT,
+  PRIMARY KEY(ID))
+  ENGINE = MyISAM; 
