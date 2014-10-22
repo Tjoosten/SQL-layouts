36ef4e4917655c4882722abee3d11301d86d3c03
Tim Joosten <Tjoosten@users.noreply.github.com>
2014-10-22 13:17:34 +0200

    Update PHP info database.sql

--- PHP info database.sql
+++ PHP info database.sql
@@ -1,9 +1,14 @@
 create database if not exists PHP_info;
 
-create table if not exists PHP_Mysqli (
+create table if not exists PHP_functions (
 	ID int(11) AUTO_INCREMENT,
 	Func_Procedural varchar(255),
 	Func_oo varchar(255), 
+	Tag varchar(55),
 	Description TEXT,
 	PRIMARY KEY(ID))
 	ENGINE = MyISAM;  
+	
+create table if not exists PHP_tags (
+	)
+	ENGINE = MyISAM;
